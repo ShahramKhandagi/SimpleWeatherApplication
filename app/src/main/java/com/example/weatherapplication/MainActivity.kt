134600c68db8d913dde47d209b604e2fe0ea469b
hp <shahramofficial2003@gmail.com>
2021-09-08 23:40:57 +0430

    Add Reload Data Option

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.weatherapplication
 
+import android.R
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
+import android.view.View
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.bumptech.glide.Glide
 import com.example.weatherapplication.databinding.ActivityMainBinding
 import okhttp3.*
 import org.json.JSONObject
@@ -15,6 +19,7 @@ import java.io.IOException
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -24,116 +29,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getCurrentCalenderAndTime()
+        getData()
 
 
-
-
-        //=========================================================================
-        // get current date
-        val year = Calendar.getInstance().get(Calendar.YEAR)
-        val monthFormatter = SimpleDateFormat("MM")
-        val currentMonth = monthFormatter.format(Date())
-        val day = LocalDate.now().getDayOfWeek().name
-
-        if (day == "SATURDAY") {
-            binding.textViewDay.text = "شنبه"
-        } else if (day == "SUNDAY") {
-            binding.textViewDay.text = "یکشنبه"
-        } else if (day == "MONDAY") {
-            binding.textViewDay.text = "دوشنبه"
-        } else if (day == "TUESDAY") {
-            binding.textViewDay.text = "سه شنبه"
-        } else if (day == "WEDNESDAY") {
-            binding.textViewDay.text = "چهارشنبه"
-        } else if (day == "THURSDAY") {
-            binding.textViewDay.text = "پنجشنبه"
-        } else if (day == "FRIDAY") {
-            binding.textViewDay.text = "جمعه"
-        }
-        
-        binding.textViewCalender.text = "$currentMonth $year"
-        val simpleDateFormatter = SimpleDateFormat("HH:mm")
-        val currentTime = simpleDateFormatter.format(Date())
-        binding.textViewCurrentTime.text = currentTime
-
-
-
-        //=========================================================================
-
-
-        val client = OkHttpClient()
-        val request = Request.Builder()
-                .url("https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=008f62bb7acf946eb8be7d3a2f3c282b&lang=fa&units=metric")
-                .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-                Log.d("tagx", "onFailure: Failed")
-            }
-
-            @RequiresApi(Build.VERSION_CODES.N)
-            override fun onResponse(call: Call, response: Response) {
-                val rowContent = response.body!!.string()
-                val jsonObject = JSONObject(rowContent)
-
-                val weatherArray = jsonObject.getJSONArray("weather")
-                val weatherObject = weatherArray.getJSONObject(0)
-
-                val iconId = weatherObject.getString("icon")
-                val imageUrl = "http://openweathermap.org/img/wn/${iconId}@2x.png"
-                val temp = jsonObject.getJSONObject("main").getDouble("temp")
-
-                //==================================================================================
-                // round Temperature number
-                val number3digits: Double = String.format("%.3f", temp).toDouble()
-                val number2digits: Double = String.format("%.2f", number3digits).toDouble()
-                val number1digits: Double = String.format("%.1f", number2digits).toDouble()
-                val roundTemperature: Double = String.format("%.0f", number1digits).toDouble()
-
-                //==================================================================================
-
-
-                val sunRise = jsonObject.getJSONObject("sys").getInt("sunrise")
-                val sunSet = jsonObject.getJSONObject("sys").getInt("sunset")
-
-                val tempMin = jsonObject.getJSONObject("main").getDouble("temp_min")
-                val tempMax = jsonObject.getJSONObject("main").getDouble("temp_max")
-
-                val airPressure = jsonObject.getJSONObject("main").getInt("pressure")
-                val humidity = jsonObject.getJSONObject("main").getInt("humidity")
-
-                val windDeg = jsonObject.getJSONObject("wind").getInt("deg")
-                val windSpeed = jsonObject.getJSONObject("wind").getDouble("speed")
-
-
-
-
-
-                runOnUiThread {
-                    showValue(
-                            jsonObject.getString("name"),
-                            weatherObject.getString("description"),
-                            roundTemperature,
-                            imageUrl,
-                            sunRise,
-                            sunSet,
-                            tempMin,
-                            tempMax,
-                            airPressure,
-                            humidity,
-                            windDeg,
-                            windSpeed
-                    )
-                }
-            }
-
-        })
 
     }
 
     @SuppressLint("ResourceAsColor")
-    fun showValue(
+    private fun showValue(
             cityName: String,
             description: String,
             temp: Double,
@@ -145,10 +49,10 @@ class MainActivity : AppCompatActivity() {
             airPressure: Int,
             humidity: Int,
             windDeg: Int,
-            windSpeed: Double
+            windSpeed: Double,
+            feelLike: Double,
+            cloudiness: Int
     ) {
-
-
         binding.textViewCityName.text = cityName
         binding.textViewWeatherDescription.text = description
         binding.textViewTemp.text = temp.toString() + "°"
@@ -160,8 +64,13 @@ class MainActivity : AppCompatActivity() {
         binding.textViewHumidity.text = humidity.toString()
         binding.textViewWindDeg.text = windDeg.toString()
         binding.textViewWindSpeed.text = windSpeed.toString()
-
-
+        binding.textViewFeelLike.text = feelLike.toString()
+        binding.textViewCloudiness.text = cloudiness.toString() + "%"
+        // Glide : Upload Weather Icon
+        Glide
+                .with(this)
+                .load(imageUrl)
+                .into(binding.imageViewWeatherIcon)
 
 
 
@@ -178,26 +87,164 @@ class MainActivity : AppCompatActivity() {
         val simpleDateFormatter = SimpleDateFormat("HH")
         val currentHour = simpleDateFormatter.format(Date()).toInt()
         if (currentHour >= hourFormatterSunSet || currentHour <= hourFormatterSunRise) {
-            binding.mainPaper.setBackgroundResource(R.drawable.nightlandscapewallpaper)
+            binding.mainPaper.setBackgroundResource(com.example.weatherapplication.R.drawable.nightlandscapewallpaper)
             binding.textViewCurrentTime.setTextColor(Color.parseColor("#FFFFFFFF"))
             binding.textViewCityName.setTextColor(Color.parseColor("#FFFFFFFF"))
             binding.textViewWeatherDescription.setTextColor(Color.parseColor("#FFFFFFFF"))
             binding.textViewTemp.setTextColor(Color.parseColor("#FFFFFFFF"))
+            binding.textViewTehran.setTextColor(Color.parseColor("#FFFFFFFF"))
+            binding.textViewTabriz.setTextColor(Color.parseColor("#FFFFFFFF"))
+            binding.textViewMashhad.setTextColor(Color.parseColor("#FFFFFFFF"))
+
         }
         //===================================================================================================
-
-
-
-//        Glide.with(this).load(imageUrl).into(binding.)
-
     }
 
-    fun getTimeFromUnixTime(unixTime: Int): String {
+    private fun getTimeFromUnixTime(unixTime: Int): String {
         val time = unixTime * 1000.toLong()
         val date = Date(time)
         val formatter = SimpleDateFormat("HH:mm")
         return formatter.format(date)
     }
 
+
+    private fun getData() {
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=008f62bb7acf946eb8be7d3a2f3c282b&lang=fa&units=metric")
+                .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                Log.d("tagx", "onFailure: Failed")
+            }
+
+            @RequiresApi(Build.VERSION_CODES.N)
+            override fun onResponse(call: Call, response: Response) {
+                val rowContent = response.body!!.string()
+                getDataAndShowThem(rowContent)
+                binding.progressBar.visibility = View.INVISIBLE
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    getCurrentCalenderAndTime()
+                }
+            }
+
+        })
+    }
+
+
+    private fun getDataAndShowThem(rawData: String) {
+
+        val jsonObject = JSONObject(rawData)
+        val weatherArray = jsonObject.getJSONArray("weather")
+        val weatherObject = weatherArray.getJSONObject(0)
+        val iconId = weatherObject.getString("icon")
+        val imageUrl = "https://openweathermap.org/img/wn/${iconId}@2x.png"
+        val temp = jsonObject.getJSONObject("main").getDouble("temp")
+
+        //==================================================================================
+        // round Temperature number
+        val number3digits: Double = String.format("%.3f", temp).toDouble()
+        val number2digits: Double = String.format("%.2f", number3digits).toDouble()
+        val number1digits: Double = String.format("%.1f", number2digits).toDouble()
+        val roundTemperature: Double = String.format("%.0f", number1digits).toDouble()
+
+        //==================================================================================
+
+
+        val sunRise = jsonObject.getJSONObject("sys").getInt("sunrise")
+        val sunSet = jsonObject.getJSONObject("sys").getInt("sunset")
+        val tempMin = jsonObject.getJSONObject("main").getDouble("temp_min")
+        val tempMax = jsonObject.getJSONObject("main").getDouble("temp_max")
+        val airPressure = jsonObject.getJSONObject("main").getInt("pressure")
+        val humidity = jsonObject.getJSONObject("main").getInt("humidity")
+        val windDeg = jsonObject.getJSONObject("wind").getInt("deg")
+        val windSpeed = jsonObject.getJSONObject("wind").getDouble("speed")
+        val feelLike = jsonObject.getJSONObject("main").getDouble("feels_like")
+        val cloudiness = jsonObject.getJSONObject("clouds").getInt("all")
+
+
+
+        runOnUiThread {
+            showValue(
+                    jsonObject.getString("name"),
+                    weatherObject.getString("description"),
+                    roundTemperature,
+                    imageUrl,
+                    sunRise,
+                    sunSet,
+                    tempMin,
+                    tempMax,
+                    airPressure,
+                    humidity,
+                    windDeg,
+                    windSpeed,
+                    feelLike,
+                    cloudiness
+            )
+        }
+
+    }
+
+
+    fun reloadData(view: View) {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.textViewCityName.text = "---"
+        binding.textViewWeatherDescription.text = "---"
+        binding.textViewTemp.text = "---"
+        binding.textViewSunRise.text = "---"
+        binding.textViewSunSet.text = "---"
+        binding.textViewTemperaturedown.text = "---"
+        binding.textViewTemperatureup.text = "---"
+        binding.textViewAirPressure.text = "---"
+        binding.textViewHumidity.text = "---"
+        binding.textViewWindDeg.text = "---"
+        binding.textViewWindSpeed.text = "---"
+        binding.textViewFeelLike.text = "---"
+        binding.textViewCloudiness.text = "---"
+        binding.textViewCalender.text = "---"
+        binding.textViewDay.text = "---"
+        binding.textViewCurrentTime.text = "--:--"
+        binding.imageViewWeatherIcon.setImageResource(com.example.weatherapplication.R.drawable.ic_refresh_24)
+
+
+        getData()
+
+
+
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getCurrentCalenderAndTime() {
+        //=========================================================================
+        // get current date
+        val year = Calendar.getInstance().get(Calendar.YEAR)
+        val monthFormatter = SimpleDateFormat("MM")
+        val currentMonth = monthFormatter.format(Date())
+        val day = LocalDate.now().getDayOfWeek().name
+        if (day == "SATURDAY") {
+            binding.textViewDay.text = "شنبه"
+        } else if (day == "SUNDAY") {
+            binding.textViewDay.text = "یکشنبه"
+        } else if (day == "MONDAY") {
+            binding.textViewDay.text = "دوشنبه"
+        } else if (day == "TUESDAY") {
+            binding.textViewDay.text = "سه شنبه"
+        } else if (day == "WEDNESDAY") {
+            binding.textViewDay.text = "چهارشنبه"
+        } else if (day == "THURSDAY") {
+            binding.textViewDay.text = "پنجشنبه"
+        } else if (day == "FRIDAY") {
+            binding.textViewDay.text = "جمعه"
+        }
+        binding.textViewCalender.text = "$currentMonth $year"
+        val simpleDateFormatter = SimpleDateFormat("HH:mm")
+        val currentTime = simpleDateFormatter.format(Date())
+        binding.textViewCurrentTime.text = currentTime
+        //=========================================================================
+    }
 
 }
